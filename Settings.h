fa9e8b65e42bb335481263f7e74e87749e9da0e6
Mayel <mayelsimon00@gmail.com>
2024-09-13 14:13:49 -0500

    First build of the project, added Source.cpp and Settings.h

---
+++ Settings.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <conio.h>
+#include <stdio.h>
+#include <string>
+#include <algorithm>
+
+using namespace System;
+using std::string;
