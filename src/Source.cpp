e101362d1d3d0c5e54e18be7e3acac0eb185a2cf
Mayel <mayelsimon00@gmail.com>
2024-09-13 20:34:04 -0500

    Actualización del source, headers añadidos

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,1 +1,10 @@
-#include "../Settings.h"
+#include <iostream>
+#include "../Headers/Settings.h"
+#include "../Headers/Interfaz.h"
+
+int main() {
+
+	main_menu();
+	system("pause>0");
+	return 0;
+}
